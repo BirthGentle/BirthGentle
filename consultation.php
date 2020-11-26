a9d774d4c14b41d4befc5d3e0980ad943df8b00f
BirthGentle <BirthGentle@gmail.com>
2020-11-26 21:36:05 +0530

    first commit

---
+++ consultation.php
@@ -0,0 +1,16 @@
+﻿<?php
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$appointment = $_REQUEST['appointment'];
+
+if ((empty($name)||empty($email)||empty($appointment)))
+{
+    echo "Please fill up all the fields";
+}
+else
+{
+    mail("mdsiddiqui17@gmail.com", "appointment day", $appointment, "from: $name<$email>");
+    echo "<script type='text/javascript'>alert('appointment timing will be contacted through mail');
+    window.history.log(-1)</script>";
+}
+?>
